07e5c00783caa4577016dcb6677bec7dde645ace
mni_Ya <qwe445176468@126.com>
2015-10-29 14:15:52 +0800

    aspectjs项目代码

---
+++ README.md
@@ -0,0 +1,99 @@
+#aspectjs切面编程
+aspectjs是一个给JavaScript增加切面功能的强大组件。这个项目起源于eco.js切面功能，但是为了让切面编程更好的发展，所以决定把这部分单独拿出来，你可以单独使用切面功能。，当然你你使用eco.js可以获得同样的功能,两个项目的功能会保持同步更新。
+
+##切面概念
+
+AOP(Aspect Oriented Programming)面向切面编程，是一种编程范式，提供从另一个角度来考虑程序结构以完善面向对象编程（OOP），AOP为开发者提供了一种描述横切关注点的机制，并能够自动将横切关注点织入到面向对象的软件系统中，从而实现了横切关注点的模块化。
+
+**方面/切面（Aspect）**：横切关注点的模块化，比日志组件。可以认为是增强、切入点的组合；在AOP中表示为“在哪里做和做什么集合”；
+**连接点（Joinpoint）**：表示需要在程序中插入横切关注点的扩展点，连接点可能是类初始化、方法执行、方法调用、字段调用或处理异常等等，aspectjs只支持方法执行连接点，在AOP中表示为“在哪里做”；
+**切入点（Pointcut）**：选择一组相关连接点的模式，即可以认为连接点的集合，在AOP中表示为“在哪里做的集合”；
+**增强（Advice）**：或称为增强在连接点上执行的行为，增强提供了在AOP中需要在切入点所选择的连接点处进行扩展现有行为的手段；包括前置增强（before advice）、后置增强 (after advice)、环绕增强 （around advice）等。在AOP中表示为“做什么”；
+**目标对象（Target Object）**：需要被织入横切关注点的对象，即该对象是切入点选择的对象，需要被增强的对象，从而也可称为“被增强对象”；在AOP中表示为“对谁做”；
+
+
+
+**aspect**： Aspect 声明类似于 Java 中的类声明，在 Aspect 中会包含着一些 Pointcut 以及相应的 Advice。
+
+**joinpoint**：表示在程序中明确定义的点，典型的包括方法调用，对类成员的访问以及异常处理程序块的执行等等，它自身还可以嵌套其它 joint point。
+
+**pointcut**：表示一组 joint point，这些 joint point 或是通过逻辑关系组合起来，或是通过通配、正则表达式等方式集中起来，它定义了相应的 Advice 将要发生的地方。
+
+**advice**：Advice 定义了在 pointcut 里面定义的程序点具体要做的操作，它通过 before、after 和 around 来区别是在每个 joint point 之前、之后还是代替执行的代码。
+
+##切面api
+
+###切面
+切面定义：`new Aspect(aspectId,advice)`。
+
+**aspectId**:切面的名称，必填。
+
+**advice**:通知，选填。
+
+例子：
+
+	var statTimesFilter = new Aspect("statTimesFilter");
+
+###通知
+我们的通知有下面几种类型：before,after,throwing,around,[eventName]。eventName可以是任何事件名.通知可以在切面定义的时候传入，也可以以后定义。
+
+通知定义：`[aspectName].advice.[adviceName] = function(jointPoint)`。
+**jointPoint**:这个连接点的数据对象。结构如下：
+
+	{
+	  context: context,  //目标对象
+	  contextName: Aspect.getClassName(context), //目标对象名字
+	  target: originTarget,  //切面针对的函数，在未加切面原来的样子
+	  targetName: targetName,//函数名字
+	  arguments: arguments,//函数参数
+	  result: "",          //函数返回的数据
+	  error: "",           //针对异常切面，返回的异常数据
+	  stop: false,         //用于阻断函数执行
+	  eventDatas: {}	   //key是某个特定事件切面的名称，value是事件传送的数据
+	}
+	
+例子：
+
+	statTimesFilter.advice.before = function(jointPoint){
+		var context = jointPoint.context;
+		var contextName = jointPoint.contextName;
+		var target = jointPoint.target;
+		var targetName = jointPoint.targetName;
+	    var arguments = jointPoint.arguments;
+		this.startTime = new Date();
+		this.escape = 0;
+		console.log("统计开始时间："+this.startTime);
+		//用于控制代码是否停止
+		// jointPoint.stop = true;
+		jointPoint.arguments[0] ="change";
+	}
+
+
+###连接点
+
+切入点是一组连接点，用于连接目标对象和切面的。
+
+切入点定义：`pointCut(context,targetNames)`。
+
+**context**:目标对象。
+
+**targetNames**:目标对象上要被加上切面的方法名。类型是array或者string
+
+参数可以是1个，也可以是两个。参数为1个的时候，且类型为array或者string，这个参数就是targetNames。如果是object，那么就是context，这个时候的targetNames为这个context上的所有方法。
+
+例子：
+
+	var testObj ={
+		f1:function(arg1){
+			console.log(arg1)
+			console.log("f1");
+			if(this._eventAdvices&&this._eventAdvices["f1"]){ //这里是针对事件切面的
+				var eventAdvice  = this._eventAdvices["f1"];
+				var eventData = {aa:"111"};
+				eventAdvice.emit("eventName",eventData);
+			}
+		}
+	}
+	statTimesFilter.pointCut(testObj,"f1");
+	statTimesFilter.pointCut(testObj,"f1");
+	testObj.f1("入参1");
---
+++ src/aspect.js
@@ -0,0 +1,199 @@
+(function(global) {
+  var util = {};
+  util.isType = function(type) {
+    return function(obj) {
+      return {}.toString.call(obj) == "[object " + type + "]"
+    }
+  }
+  util.isObject = util.isType("Object");
+  util.isString = util.isType("String");
+  util.isArray = Array.isArray || util.isType("Array");
+  util.isFunction = util.isType("Function");
+  util.isUndefined = util.isType("Undefined");
+  util.isInArray = function(array, item) {
+      for (i = 0; i < array.length; i++) {
+        if (array[i] == item)
+          return true;
+      }
+      return false;
+  }
+    //对象扩充
+  util.extend = function(target, obj, deep, included) {
+    var key;
+    if (!obj) return target;
+    if (util.isArray(deep)) {
+      included = deep;
+      deep = false;
+    }
+    if (util.isArray(included) && included.length == 0) included = false;
+    for (key in obj) {
+      if (util.isArray(included) && !util.isInArray(included, key)) continue;
+      if (target === obj[key]) continue;
+      if (util.isObject(obj[key]) && deep) {
+        if (!target[key]) target[key] = {};
+        util.extend(target[key], obj[key], deep);
+      }
+      target[key] = obj[key];
+    }
+    return target;
+  };
+
+  //摻元事件对象
+  var event = {
+    events: {},
+    contexts: {}
+  };
+
+  event.on = function(name, callback, context) {
+    var list = this.events[name] || (this.events[name] = [])
+    list.push(callback)
+    return this
+  }
+
+  event.one = function(name, callback) {
+    var self = this;
+    var oneHandler = function(data) {
+      self.off(name, oneHandler);
+      callback.call(self, data);
+    };
+    self.on(name, oneHandler);
+    return this
+  }
+
+  event.emit = function(name, data) {
+    var list = this.events[name]
+    if (list) {
+      list = list.slice()
+
+      for (var i = 0, len = list.length; i < len; i++) {
+        list[i](data)
+      }
+    }
+    return this
+  };
+
+  event.off = function(name, callback) {
+    if (!(name || callback)) {
+      this.events = {}
+      return this
+    }
+
+    var list = this.events[name]
+    if (list) {
+      if (callback) {
+        for (var i = list.length - 1; i >= 0; i--) {
+          if (list[i] === callback) {
+            list.splice(i, 1)
+          }
+        }
+      } else {
+        delete this.events[name]
+      }
+    }
+    return this
+  };
+
+  //切面类
+  function Aspect(id, advice) {
+    var aspect = {};
+    aspect.id = id;
+    aspect.advice = advice ? advice : {};
+    aspect.pointCut = Aspect.pointCut;
+    aspect._wrapperFunc = Aspect._wrapperFunc;
+    return aspect;
+  }
+
+  //获取函数名
+  Aspect.getMethodName = function(func) {
+    return func.name || func.toString().match(/function\s*([^(]*)\(/)[1];
+  }
+
+  //获取对象类型名
+  Aspect.getClassName = function(obj) {
+    return Aspect.getMethodName(obj.constructor);
+  }
+
+  //添加切面
+  Aspect.pointCut = function(context, targetNames) {
+    if (arguments.length == 1) {
+      if (!util.isObject(context)) {
+        targetNames = context;
+        context = global;
+      } else {
+        targetNames = [];
+        for (p in context) {
+          if (util.isFunction(context[p])) {
+            targetNames.push(p);
+          }
+        }
+      }
+    }
+    if (util.isString(targetNames)) {
+      targetNames = [targetNames];
+    }
+    for (var i = 0; i < targetNames.length; i++) {
+      var targetName = targetNames[i];
+      context[targetName] = this._wrapperFunc(context, targetName);
+    }
+  };
+
+  Aspect._exclused = ["before", "after", "around", "throwing"];
+  Aspect._wrapperFunc = function(context, targetName) {
+    var originTarget = context[targetName];
+    //扩充
+    var advice = util.extend({}, this.advice);
+    if (!context._eventAdvices) context._eventAdvices = {};
+    if (!context._eventAdvices[targetName]) {
+      context._eventAdvices[targetName] = util.extend({}, event, true);
+    }
+    var eventAdvice = context._eventAdvices[targetName];
+    return function() {
+      //添加事件通知
+      for (p in advice) {
+        if (util.isFunction(advice[p]) && !util.isInArray(Aspect._exclused, p)) {
+          eventAdvice.on(p, function(eventData) {
+            advice.joinPoint.eventDatas[p] = eventData;
+            advice[p].call(advice, advice.joinPoint);
+          });
+        }
+      }
+
+      try {
+        var joinPoint = {
+          context: context,
+          contextName: Aspect.getClassName(context),
+          target: originTarget,
+          targetName: targetName,
+          arguments: arguments,
+          result: "",
+          error: "",
+          stop: false,
+          eventDatas: {}
+        };
+        advice.joinPoint = joinPoint;
+        if (!eventAdvice.joinPoint) eventAdvice.joinPoint = joinPoint;
+        if (advice.before) {
+          advice.before(joinPoint);
+          if (joinPoint.stop) return;
+        }
+        if (advice.around) {
+          advice.around(joinPoint);
+          if (joinPoint.stop) return;
+        } else {
+          joinPoint.result = originTarget.apply(context, arguments);
+        }
+        if (advice.after) {
+          advice.after(joinPoint);
+          if (joinPoint.stop) return;
+        }
+      } catch (error) {
+        if (advice.throwing) {
+          advice.throwing(joinPoint);
+        }
+      }
+      return joinPoint.result;
+    }
+  }
+  global.Aspect = Aspect;
+
+})(this);
